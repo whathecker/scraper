d8579ef7f616b7807ae48d8f53e916559cdf11da
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-01-19 19:45:53 +0100

    chore: add dependencies

---
+++ worker/src/config/index.ts
@@ -0,0 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  MAIN_SEARCH_URL: process.env.MAIN_SEARCH_URL,
+};
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,1 +1,4 @@
+import config from './config';
+
+console.log(config.MAIN_SEARCH_URL);
 console.log('hey working is running');
