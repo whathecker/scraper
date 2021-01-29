a6ff50a05ae4cf974261aa939e997ac9cfc2e106
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-01-29 20:11:22 +0100

    build: support start and stop of the child process

--- worker/src/frameworks/web/apis/index.ts
+++ worker/src/frameworks/web/apis/index.ts
@@ -1,33 +1,35 @@
 import * as express from 'express';
-import { fork } from 'child_process';
+import { ChildProcess, fork } from 'child_process';
 import redis from 'redis';
 import RedisAccessor from '../../../redis-accessor/redis-accessor.impl';
 
 const redisAccessor = new RedisAccessor(redis);
 const router: express.Router = express.Router();
 
+let childProcess: ChildProcess | null = null;
+
 router.get('/process/totalcount/:searchTerm', async (req: express.Request, res: express.Response) => {
   console.log(req.params);
-  // spwan child process
-  // wait until child process return the total count
-  // return totalcount to client
   return res.status(200).end();
 });
 
 router.post('/process/start', async (_req: express.Request, res: express.Response) => {
-  const childProcess = fork(__dirname + '../../../../email-collector/email-collector.impl');
-  console.log(childProcess);
-  // this request should be a websocket endpoint
-  // start the child process and instruct worker to start scraping
-  // inform client when requeest stop, finished, errored
-  // merge put endpoint into this post endpoint (i.e., get input to stop the process as if the server is getting the chat message from client)
-  return res.status(200).end();
+  if (childProcess === null) {
+    childProcess = fork(__dirname + '../../../../email-collector/email-collector.impl');
+    return res.status(200).end();
+  } else {
+    return res.status(400).json('Bad request: process already running');
+  }
 });
 
-router.put('/process/stop', async (req: express.Request, res: express.Response) => {
-  // this request might not needed
-  console.log(req);
-  return res.status(200).end();
+router.put('/process/stop', async (_req: express.Request, res: express.Response) => {
+  if (childProcess !== null) {
+    childProcess.kill();
+    childProcess = null;
+    return res.status(200).json('Process has terminated');
+  } else {
+    return res.status(400).json('Bad request: no process is running');
+  }
 });
 
 router.get('/process/result', async (_req: express.Request, res: express.Response) => {
